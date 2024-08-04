73bfb91c5d4776df01fcec73883522fa92ab9c08
mark-k-mokaya <mark-k-mokaya.gmail.com>
2024-08-04 15:36:33 +0300

    Add billing and client side

--- services/passport.js
+++ services/passport.js
@@ -21,18 +21,13 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true,
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({googleId: profile.id}).then((existingUser) => {
-				if (existingUser) {
-					// already have a record with the same profile id
-					done(null, existingUser);
-				} else {
-					// no user record found, create new user
-					new User({googleId: profile.id})
-						.save()
-						.then((user) => done(null, user));
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({googleId: profile.id});
+			if (existingUser) {
+				return done(null, existingUser);
+			}
+			const user = await new User({googleId: profile.id}).save();
+			done(null, user);
 		}
 	)
 );
